dc8f463f83e5a75a75f484a7e919d790d5d661a1
su <rakdav@inbox.ru>
2022-09-24 14:14:10 +0200

    Добавьте файлы проекта.

---
+++ BridgeProject/Program.cs
@@ -0,0 +1,84 @@
+﻿Programmer freelancer = new FreelanceProgrammer(new CPPLanguage());
+freelancer.DoWork();
+freelancer.EarnMoney();
+
+freelancer.Language=new CSharpLanguage();
+freelancer.DoWork();
+freelancer.EarnMoney();
+
+Programmer corpprogrammer = new CorporateProgrammer(new CSharpLanguage());
+corpprogrammer.DoWork();
+corpprogrammer.EarnMoney();
+
+interface ILanguage
+{
+    void Build();
+    void Execute();
+}
+
+class CPPLanguage : ILanguage
+{
+    public void Build()
+    {
+        Console.WriteLine("С помощью компилятора С++ компилируем программу в бинарный код"); ;
+    }
+
+    public void Execute()
+    {
+        Console.WriteLine("Запускаем исполняемый файл программы"); ;
+    }
+}
+class CSharpLanguage : ILanguage
+{
+    public void Build()
+    {
+        Console.WriteLine("С помощью компилятора переводим в промежуточный язык CIL"); ;
+    }
+
+    public void Execute()
+    {
+        Console.WriteLine("JIT компилирует программу в бинарный код");
+        Console.WriteLine("CLR выполняет скомпилированный бинарный код");
+    }
+}
+abstract class Programmer
+{
+    protected ILanguage language;
+    public ILanguage Language
+    {
+        set { language = value; }
+    }
+    protected Programmer(ILanguage language)
+    {
+        this.language = language;
+    }
+    public virtual void DoWork()
+    {
+        language.Build();
+        language.Execute();
+    }
+    public abstract void EarnMoney();
+}
+class FreelanceProgrammer : Programmer
+{
+    public FreelanceProgrammer(ILanguage language) : base(language)
+    {
+    }
+
+    public override void EarnMoney()
+    {
+        Console.WriteLine("Получаем оплату за выполненный заказ");
+    }
+}
+
+class CorporateProgrammer : Programmer
+{
+    public CorporateProgrammer(ILanguage language) : base(language)
+    {
+    }
+
+    public override void EarnMoney()
+    {
+        Console.WriteLine("Получить зарплату в конце месяца"); ;
+    }
+}
---
+++ StructPatterns/Program.cs
@@ -0,0 +1,50 @@
+﻿Driver driver = new Driver();
+Auto auto = new Auto();
+driver.Travel(auto);
+Camel camel= new Camel();
+ITransport camelTransport = new CamelToTransportAdapter(camel);
+driver.Travel(camelTransport);
+
+interface ITransport
+{
+    void Drive();
+}
+class Auto : ITransport
+{
+    public void Drive()
+    {
+        Console.WriteLine("Машина едет по дороге");
+    }
+}
+class Driver
+{
+    public void Travel(ITransport transport)
+    {
+        transport.Drive();
+    }
+}
+interface ICamel
+{
+    void Move();
+}
+class Camel : ICamel
+{
+    public void Move()
+    {
+        Console.WriteLine("Верблюд едет по пустыне");
+    }
+}
+class CamelToTransportAdapter:ITransport
+{
+    Camel camel;
+
+    public CamelToTransportAdapter(Camel camel)
+    {
+        this.camel = camel;
+    }
+
+    public void Drive()
+    {
+        camel.Move();
+    }
+}
